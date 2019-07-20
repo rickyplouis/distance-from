142767acd0778857708d878d550aae4a9f78f0b9
AIRTucha <airtucha@icloud.com>
2019-07-20 09:57:49 +0200

    Restore original formating of tests

--- __tests__/distance.spec.js
+++ __tests__/distance.spec.js
@@ -1,41 +1,41 @@
-const distFrom = require('../dist/index')
+const distFrom = require('../dist/index');
 
-const ny = [40.71278, -74.00594]
-const london = [51.50853, -0.12574]
+const ny = [40.71278, -74.00594];
+const london = [51.50853, -0.12574];
 
 describe('distFrom().from().to()', () => {
   test('throws for no origin', () => {
     expect(() => {
-      distFrom().to(london)
-    }).toThrow()
-  })
+      distFrom().to(london);
+    }).toThrow();
+  });
 
   test('throws for no destination', () => {
     expect(() => {
-      distFrom(ny).to()
-    }).toThrow()
-  })
+      distFrom(ny).to();
+    }).toThrow();
+  });
 
   test('throws for bad origin', () => {
     expect(() => {
-      distFrom('my house').to(london)
-    }).toThrow()
-  })
+      distFrom('my house').to(london);
+    }).toThrow();
+  });
 
   test('throws for bad destination', () => {
     expect(() => {
-      distFrom(ny).to('my house')
-    }).toThrow()
-  })
-})
+      distFrom(ny).to('my house');
+    }).toThrow();
+  });
+});
 
 describe('distFrom().in()', () => {
   test('gets distance without destination in miles', () => {
-    expect( () => { 
-      distFrom(ny).in('miles')
-    }).toThrow()
-  })
-})
+    expect(() => {
+      distFrom(ny).in('miles');
+    }).toThrow();
+  });
+});
 
 describe('distFrom().from().to().in()', () => {
   test('gets distance from ny to london in kilometers', () => {
@@ -46,8 +46,8 @@ describe('distFrom().from().to().in()', () => {
           .in('km'),
         10
       )
-    ).toBe(5570)
-  })
+    ).toBe(5570);
+  });
 
   test('gets distance from ny to london in miles', () => {
     expect(
@@ -57,8 +57,8 @@ describe('distFrom().from().to().in()', () => {
           .in('miles'),
         10
       )
-    ).toBe(3461)
-  })
+    ).toBe(3461);
+  });
 
   test('gets distance from ny to london in feet', () => {
     expect(
@@ -68,70 +68,69 @@ describe('distFrom().from().to().in()', () => {
           .in('ft'),
         10
       )
-    ).toBe(18275313)
-  })
+    ).toBe(18275313);
+  });
 
-  test('gets distance from ny to london in meters', () => {		
-    expect(		
-      parseInt(		
-        distFrom(ny)		
-          .to(london)		
-          .in('m'),		
-        10		
-      )		
-    ).toBe(5570315);		
+  test('gets distance from ny to london in meters', () => {
+    expect(
+      parseInt(
+        distFrom(ny)
+          .to(london)
+          .in('m'),
+        10
+      )
+    ).toBe(5570315);
   });
 
   test('test invalid units', () => {
     expect(() => {
       distFrom(ny)
         .to(london)
-        .in('steps')
-    }).toThrow()
-  })
-  
-})
+        .in('steps');
+    }).toThrow();
+  });
+});
 
 describe('distFrom().validUnits()', () => {
   test('throws with non string input', () => {
     expect(() => {
-      distFrom().validUnits(true)
-    }).toThrow()
-  })
+      distFrom().validUnits(true);
+    }).toThrow();
+  });
 
   test('throws with no input', () => {
     expect(() => {
-      distFrom().validUnits()
-    }).toThrow()
-  })
+      distFrom().validUnits();
+    }).toThrow();
+  });
 
   test('returns feet as valid unit', () => {
-    expect(distFrom().validUnits('feet')).toBeTruthy()
-  })
+    expect(distFrom().validUnits('feet')).toBeTruthy();
+  });
 
   test('returns steps as invalid unit', () => {
-    expect(distFrom().validUnits('steps')).toBeFalsy()
-  })
-})
+    expect(distFrom().validUnits('steps')).toBeFalsy();
+  });
+});
 
 describe('distFrom().degreeToRadians', () => {
   test('degree to radians with no input', () => {
     expect(() => {
-      distFrom().degreeToRadians('six')
-    }).toThrow()
-  })
+      distFrom().degreeToRadians('six');
+    }).toThrow();
+  });
 
   test('degree to radians with no input', () => {
-    expect(distFrom().degreeToRadians()).toEqual(0)
-  })
+    expect(distFrom().degreeToRadians()).toEqual(0);
+  });
 
   test('degree to radians with standard input', () => {
-    expect(parseInt(distFrom().degreeToRadians(100), 10)).toEqual(1)
-  })
-})
+    expect(parseInt(distFrom().degreeToRadians(100), 10)).toEqual(1);
+  });
+});
 
 describe('distFrom().unitList()', () => {
   test('gets list of units', () => {
-    expect(Array.isArray(distFrom().unitList())).toBe(true)
-  })
-})
+    expect(Array.isArray(distFrom().unitList())).toBe(true);
+  });
+});
