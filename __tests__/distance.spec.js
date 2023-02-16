2b4658d3cd4b0aaa1b592910e999ddff2e9ebecc
Dimitri B <dimitri.benin@gmail.com>
2023-02-16 01:21:23 +0100

    Fix TypeScript types
    - expose types and fix exports so that a sensible .d.ts is generated
    - add a types field to expose generated .d.ts for TypeScript
    - upgrade dev dependencies
    - switch to eslint to replace deprecated tslint
    - upgrade husky config to work with newest version

--- __tests__/distance.spec.js
+++ __tests__/distance.spec.js
@@ -1,175 +1,124 @@
-const distFrom = require('../dist/index');
+const distFrom = require('../dist/index')
 
-const ny = [40.71278, -74.00594];
-const london = [51.50853, -0.12574];
+const ny = [40.71278, -74.00594]
+const london = [51.50853, -0.12574]
 
 describe('distFrom().from().to()', () => {
   test('throws for no origin', () => {
     expect(() => {
-      distFrom().to(london);
-    }).toThrow();
-  });
+      distFrom().to(london)
+    }).toThrow()
+  })
 
   test('throws for no destination', () => {
     expect(() => {
-      distFrom(ny).to();
-    }).toThrow();
-  });
+      distFrom(ny).to()
+    }).toThrow()
+  })
 
   test('throws for bad origin', () => {
     expect(() => {
-      distFrom('my house').to(london);
-    }).toThrow();
-  });
+      distFrom('my house').to(london)
+    }).toThrow()
+  })
 
   test('throws for bad destination', () => {
     expect(() => {
-      distFrom(ny).to('my house');
-    }).toThrow();
-  });
-});
+      distFrom(ny).to('my house')
+    }).toThrow()
+  })
+})
 
 describe('distFrom().in()', () => {
   test('gets distance without destination in miles', () => {
     expect(() => {
-      distFrom(ny).in('miles');
-    }).toThrow();
-  });
-});
+      distFrom(ny).in('miles')
+    }).toThrow()
+  })
+})
 
 describe('distFrom().from().to().in()', () => {
   test('gets distance from ny to london in kilometers', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('km'),
-        10
-      )
-    ).toBe(5570);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('km'), 10)).toBe(5570)
+  })
 
   test('gets distance from ny to london in meters', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('m'),
-        10
-      )
-    ).toBe(5570315);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('m'), 10)).toBe(5570315)
+  })
 
   test('gets distance from ny to london in centimeters', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('cm'),
-        10
-      )
-    ).toBe(557031545);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('cm'), 10)).toBe(557031545)
+  })
 
   test('gets distance from ny to london in miles', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('miles'),
-        10
-      )
-    ).toBe(3461);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('miles'), 10)).toBe(3461)
+  })
 
   test('gets distance from ny to london in yards', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('yards'),
-        10
-      )
-    ).toBe(6091752);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('yards'), 10)).toBe(6091752)
+  })
 
   test('gets distance from ny to london in kilometers', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('inches'),
-        10
-      )
-    ).toBe(219303876);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('inches'), 10)).toBe(219303876)
+  })
 
   test('gets distance from ny to london in feet', () => {
-    expect(
-      parseInt(
-        distFrom(ny)
-          .to(london)
-          .in('ft'),
-        10
-      )
-    ).toBe(18275313);
-  });
+    expect(parseInt(distFrom(ny).to(london).in('ft'), 10)).toBe(18275313)
+  })
 
   test('gets distance from ny to london in two twice', () => {
-    const dist = distFrom(ny).to(london);
-    expect(parseInt(dist.in('m'), 10)).toBe(5570315);
-    expect(parseInt(dist.in('m'), 10)).toBe(5570315);
-  });
+    const dist = distFrom(ny).to(london)
+    expect(parseInt(dist.in('m'), 10)).toBe(5570315)
+    expect(parseInt(dist.in('m'), 10)).toBe(5570315)
+  })
 
   test('test invalid units', () => {
     expect(() => {
-      distFrom(ny)
-        .to(london)
-        .in('steps');
-    }).toThrow();
-  });
-});
+      distFrom(ny).to(london).in('steps')
+    }).toThrow()
+  })
+})
 
 describe('distFrom().validUnits()', () => {
   test('throws with non string input', () => {
     expect(() => {
-      distFrom().validUnits(true);
-    }).toThrow();
-  });
+      distFrom().validUnits(true)
+    }).toThrow()
+  })
 
   test('throws with no input', () => {
     expect(() => {
-      distFrom().validUnits();
-    }).toThrow();
-  });
+      distFrom().validUnits()
+    }).toThrow()
+  })
 
   test('returns feet as valid unit', () => {
-    expect(distFrom().validUnits('feet')).toBeTruthy();
-  });
+    expect(distFrom().validUnits('feet')).toBeTruthy()
+  })
 
   test('returns steps as invalid unit', () => {
-    expect(distFrom().validUnits('steps')).toBeFalsy();
-  });
-});
+    expect(distFrom().validUnits('steps')).toBeFalsy()
+  })
+})
 
 describe('distFrom().degreeToRadians', () => {
   test('degree to radians with no input', () => {
     expect(() => {
-      distFrom().degreeToRadians('six');
-    }).toThrow();
-  });
+      distFrom().degreeToRadians('six')
+    }).toThrow()
+  })
 
   test('degree to radians with no input', () => {
-    expect(distFrom().degreeToRadians()).toEqual(0);
-  });
+    expect(distFrom().degreeToRadians()).toEqual(0)
+  })
 
   test('degree to radians with standard input', () => {
-    expect(parseInt(distFrom().degreeToRadians(100), 10)).toEqual(1);
-  });
-});
+    expect(parseInt(distFrom().degreeToRadians(100), 10)).toEqual(1)
+  })
+})
 
 describe('distFrom().unitList()', () => {
   test('gets list of units', () => {
-    expect(Array.isArray(distFrom().unitList())).toBe(true);
-  });
-});
+    expect(Array.isArray(distFrom().unitList())).toBe(true)
+  })
+})
--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,72 @@
-import { Result, Failure, Success} from "amonad"
+import type { Result } from 'amonad'
+import { Failure, Success } from 'amonad'
 
-type Position = [number, number]
-type Units = 'km' | 'kilometer' | 'kilometers' | 'm' | 'meters' | 'meter' | 'metre' | 'cm' | 'centimeter' | 'centimeters' | 'mi' | 'mile' | 'miles' | 'feet' | 'ft' | 'in' | 'inch' | 'inches' | 'yd' | 'yard' | 'yards'
+// The following namespace is required to use declaration merging to be able to export
+// the types.
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace distanceFrom {
+  export type Position = [number, number]
+  export type Units =
+    | 'km'
+    | 'kilometer'
+    | 'kilometers'
+    | 'm'
+    | 'meters'
+    | 'meter'
+    | 'metre'
+    | 'cm'
+    | 'centimeter'
+    | 'centimeters'
+    | 'mi'
+    | 'mile'
+    | 'miles'
+    | 'feet'
+    | 'ft'
+    | 'in'
+    | 'inch'
+    | 'inches'
+    | 'yd'
+    | 'yard'
+    | 'yards'
+  export type DistanceFrom = Distance
+}
 
-const unitList: Units[] = ['km', 'kilometers', 'kilometers', 'm', 'meters', 'meter', 'metre', 'cm', 'centimeter', 'centimeters', 'mi', 'mile', 'miles', 'feet', 'ft', 'in', 'inch', 'inches', 'yd', 'yard', 'yards']
+const unitList: distanceFrom.Units[] = [
+  'km',
+  'kilometers',
+  'kilometers',
+  'm',
+  'meters',
+  'meter',
+  'metre',
+  'cm',
+  'centimeter',
+  'centimeters',
+  'mi',
+  'mile',
+  'miles',
+  'feet',
+  'ft',
+  'in',
+  'inch',
+  'inches',
+  'yd',
+  'yard',
+  'yards',
+]
+
+// The following is needed to have an alias name to be able to export the DistanceFrom
+// interface from the namespace with the same name.
+type Distance = DistanceFrom
 
 class DistanceFrom {
-  private distance: Result<number, Error> = Failure(new Error("Destination is not configured, run distFrom.to()."))
+  private distance: Result<number, Error> = Failure(
+    new Error('Destination is not configured, run distFrom.to().')
+  )
 
-  constructor( private origin: Position ) { }
+  constructor(private readonly origin: distanceFrom.Position) {}
 
-  degreeToRadians(degrees: number = 0) {
+  degreeToRadians(degrees = 0) {
     // Math.PI / 180
     if (isNaN(degrees)) {
       throw new Error('Must input valid number for degrees')
@@ -30,18 +86,20 @@ class DistanceFrom {
     const cos = (num: number) => Math.cos(num)
 
     const radius = 6371
+    /* eslint-disable @typescript-eslint/naming-convention */
     const φ1 = this.degreeToRadians(lat1)
     const λ1 = this.degreeToRadians(lon1)
     const φ2 = this.degreeToRadians(lat2)
     const λ2 = this.degreeToRadians(lon2)
     const Δφ = φ2 - φ1
     const Δλ = λ2 - λ1
+    /* eslint-enable @typescript-eslint/naming-convention */
 
     const a = sine(Δφ) * sine(Δφ) + cos(φ1) * cos(φ2) * Math.pow(sine(Δλ), 2)
     return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) * radius
   }
 
-  to( destination: Position) {
+  to(destination: distanceFrom.Position) {
     if (!Array.isArray(this.origin)) {
       throw new Error('Must use array of [lat, lng] for origin')
     }
@@ -51,52 +109,51 @@ class DistanceFrom {
     }
 
     this.distance = Success(
-      this.distanceInKm(
-        this.origin[0],
-        this.origin[1],
-        destination[0],
-        destination[1]
-      )
+      this.distanceInKm(this.origin[0], this.origin[1], destination[0], destination[1])
     )
 
     return this
   }
 
-  validUnits(unit: Units) {
+  validUnits(unit: distanceFrom.Units) {
     if (unit) {
       if (typeof unit !== 'string') {
         throw new Error('Unit must be type of string')
       }
 
-      return this.unitList().indexOf(unit) >= 0
+      return this.unitList().includes(unit)
     }
 
     throw new Error('Must input a unit to determine if valid')
   }
 
-  in( units: Units ) {
+  in(units: distanceFrom.Units) {
     return this.distance
-      .bind( distance =>
-        this.validUnits(units) ?
-          Success<number, Error>(distance)
-          :
-          Failure<number, Error>(new Error('Need to use valid units, run distFrom.unitList() to see list'))
+      .bind((distance) =>
+        this.validUnits(units)
+          ? Success<number, Error>(distance)
+          : Failure<number, Error>(
+              new Error('Need to use valid units, run distFrom.unitList() to see list')
+            )
       )
-      .bind( distance => {
+      .bind((distance) => {
         if (units === 'mi' || units === 'mile' || units === 'miles')
           return distance * 0.6213712
-        else if (units === 'm' || units === 'meter' || units === 'meters' || units === 'metre')
+        else if (
+          units === 'm' ||
+          units === 'meter' ||
+          units === 'meters' ||
+          units === 'metre'
+        )
           return distance * 1000
         else if (units === 'cm' || units === 'centimeter' || units === 'centimeters')
           return distance * 100000
         else if (units === 'in' || units === 'inch' || units === 'inches')
           return distance * 39370.1
-        else if (units === 'ft' || units === 'feet')
-          return distance * 3280.84
+        else if (units === 'ft' || units === 'feet') return distance * 3280.84
         else if (units === 'yd' || units === 'yard' || units === 'yards')
           return distance * 1093.61
-        else
-          return distance
+        else return distance
       })
       .getOrThrow()
   }
@@ -106,6 +163,8 @@ class DistanceFrom {
   }
 }
 
-module.exports = function (val: Position) {
+function distanceFrom(val: distanceFrom.Position) {
   return new DistanceFrom(val)
 }
+
+export = distanceFrom
